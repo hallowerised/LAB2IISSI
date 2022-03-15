57cff33853d69d243e7dbf4456d1ddbab489316b
hallowerised <hallowerised27@gmail.com>
2022-03-15 15:59:59 +0100

    primer commit samu

--- controllers/RestaurantController.js
+++ controllers/RestaurantController.js
@@ -2,21 +2,66 @@
 const models = require('../models')
 const Restaurant = models.Restaurant
 const Product = models.Product
+const newRestaurant = Restaurant.build(req.body)
 
 exports.index = async function (req, res) {
-
+  try {
+    const restaurants = await Restaurant.findAll()
+    res.json(restaurants)
+  } catch (err) {
+    res.status(500).send(err)
+  }
 }
 
 exports.indexOwner = function (req, res) {
-  res.sendStatus(404)
+  try {
+    const restaurants = await Restaurant.findAll({
+      where: {
+        ownerId: req.params.restaurantId
+      }
+    })
+    res.json(restaurants)
+  } catch (err) {
+    res.sendStatus(404).send(err)
+  }
+  
 }
 
 exports.create = async function (req, res) {
-
+  if (typeof req.files?.heroImage !== 'undefined') {
+    newRestaurant.heroImage = req.files.heroImage[0].path
+  }
+  if (typeof req.files?.logo !== 'undefined') {
+    newRestaurant.logo = req.files.logo[0].path
+  try {
+      const restaurant = await newRestaurant.save()
+      res.json(restaurant)
+    }
+  catch (err) {
+    if (err.name.includes('ValidationError')) { //The database may return some kind of error.
+        res.status(422).send(err)
+    } else {
+        res.status(500).send(err)
+    }
+}
+  }
 }
 
 exports.show = async function (req, res) {
-
+  try {
+    const restaurant = await Restaurant.findByPk(req.params.restaurantId,
+      {
+        include: [
+          {
+            model: Product,
+            as: 'products'
+          }
+       ]
+      })
+    res.json(restaurant)
+  } catch (err) {
+    res.status(500).send(err)
+  }
 }
 
 // Implement below, the update and destroy functions as well
--- routes/RestaurantRoutes.js
+++ routes/RestaurantRoutes.js
@@ -1,9 +1,50 @@
 'use strict'
 const RestaurantController = require('../controllers/RestaurantController')
+const ProductController = require('../controllers/ProductController')
+
 const OrderController = require('../controllers/OrderController')
+const ProductRoutes = require('./ProductRoutes')
 
 module.exports = (options) => {
   const app = options.app
+  app.route('/restaurants/owner')
+    .post(
+      RestaurantController.create
+    )
+    .get(
+      RestaurantController.index
+    )
+    .put(
+      RestaurantController.update
+    )
+    .delete(
+      RestaurantController.destroy
+    )
+  app.route('/restaurants')
+    .get(
+      RestaurantController.index
+    )
+  app.route('/restaurants/:restaurantId')
+    .get(
+      RestaurantController.show
+    )
+  app.route('/restaurants/:restaurantId/orders')
+    .get(
+      OrderController.indexRestaurant
+    )
+  
+  app.route('/restaurants/orders')
+    .get(
+      OrderController.analytics
+    )
+  
+}
+  
+
+
+
+  
+
 
   // TODO: Include routes for restaurant described in the lab session wiki page.
-}
+
